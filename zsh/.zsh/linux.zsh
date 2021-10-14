25512aad0c9c6896be50a20f5ec258d85d1e56dc
Keith Schaab <keithsc@google.com>
2021-10-14 13:05:37 -0700

    Fixing linux.zsh issue

--- zsh/.zsh/linux.zsh
+++ zsh/.zsh/linux.zsh
@@ -4,7 +4,7 @@ export PATH=$HOME/bin/share/linux:$PATH
 # Google Cloud SDK #
 ####################
 GCLOUD_SDK_DIR=$HOME/google-cloud-sdk/bin 
-if [[ -d $GCLOUD_SDK_DIR ]; then
+if [[ -d $GCLOUD_SDK_DIR ]]; then
   [[ -f "$GCLOUD_SDK_DIR/path.zsh.inc" ]] && source "$GCLOUD_SDK_DIR/path.zsh.inc"
 fi
 
