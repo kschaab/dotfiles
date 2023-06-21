713d14e9135d1439d375b3046865288214a60a97
Keith Schaab <keithsc@google.com>
2023-06-21 07:39:06 -0700

    Removing gcloud from brew so that it is maintained better

--- zsh/.zsh/mac.zsh
+++ zsh/.zsh/mac.zsh
@@ -4,8 +4,7 @@ export LD_LIBRARY_PATH=$HOME/homebrew/lib:$LD_LIBRARY_PATH
 ####################
 # Google Cloud SDK #
 ####################
-GCLOUD_SDK_DIR="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/"
-
+GCLOUD_SDK_DIR=$HOME/google-cloud-sdk/bin
 if [[ -d $GCLOUD_SDK_DIR ]]; then
   [[ -f "$GCLOUD_SDK_DIR/path.zsh.inc" ]] && source "$GCLOUD_SDK_DIR/path.zsh.inc"
 fi
